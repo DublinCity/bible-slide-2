da9640d7d5401b05de93a5768ad913aacaf6189d
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-09-21 12:03:53 +0200

    add support for react, stage-0 and legacy-decorators. Use jsprettier via eslint

---
+++ src/renderer/components/AppShell/AppShell.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AppShell extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AppShell</h1>
+            </div>
+        );
+    }
+}
