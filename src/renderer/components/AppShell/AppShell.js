29f011cebe8ccc36a18537531df58b64d7636f06
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-09-21 12:28:16 +0200

    add SASS support

--- src/renderer/components/AppShell/AppShell.js
+++ src/renderer/components/AppShell/AppShell.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import styles from './AppShell.scss';
 
 export default class AppShell extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.AppShell}>
                 <h1>AppShell</h1>
             </div>
         );
