4602ca951eaa0d77616d05ecceeba55562b88a74
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-09-22 14:32:09 +0200

    invert production vs development logic

--- scripts/patches/cssHotLoader/common.js
+++ scripts/patches/cssHotLoader/common.js
@@ -2,7 +2,7 @@
 
 const before = `configurator.extensions.push(".css");`;
 const after = `configurator.extensions.push(".css");
-        const cssHotLoader = configurator.isProduction ? ['css-hot-loader'] : [];`;
+        const cssHotLoader = configurator.isProduction ? [] : ['css-hot-loader'];`;
 
 module.exports = {
     files: 'node_modules/electron-webpack/out/targets/RendererTarget.js',
